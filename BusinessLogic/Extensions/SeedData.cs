b87d6d82ab93bfb050f47f8e9d1ced3552417a92
Chidiebere Alfred Levi <46676018+rapXidy@users.noreply.github.com>
2024-05-24 15:32:24 +0100

    push

---
+++ BusinessLogic/Extensions/SeedData.cs
@@ -0,0 +1,31 @@
+﻿using System.Data;
+
+namespace HabariShopRU.API.BusinessLogic.Extensions
+{
+    public class SeedData
+    {
+        private readonly RequestDelegate _next;
+
+        public SeedData(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context, IDbConnection dbConnection)
+        {
+            dbConnection.Open();
+
+            string createTableQuery = @"
+            IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Customers' and xtype='U')
+            CREATE TABLE Customers (
+                Id INT PRIMARY KEY IDENTITY,
+                Name NVARCHAR(100) NOT NULL,
+                Email NVARCHAR(100) NOT NULL
+            );";
+
+            dbConnection.Execute(createTableQuery);
+
+            await _next(context);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
 using HabariShopRU.API.BusinessLogic.Extensions;
+using System.Data;
+using System.Data.SqlClient;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSingleton<IDbConnection>(sp =>
+{
+    var configuration = sp.GetRequiredService<IConfiguration>();
+    var connectionString = configuration.GetConnectionString("DevConnection");
+
+    // Use SQL Server
+    return new SqlConnection(connectionString);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
